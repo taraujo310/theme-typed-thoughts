4fc1be91803bd1d2ec9a127c1788d4c650be4453
Thiago Ururay <tufa.araujo@gmail.com>
2025-03-24 03:15:59 -0300

    remove all plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,37 +15,9 @@ module.exports = {
       'title': ['"Alfa Slab One"', 'ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
       'postBody': ['"Open Sans"', 'ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"']
     },
-    markdownBase: {
-      wrapperClass: 'content',
-      h1: {
-        marginTop: '3rem',
-      },
-      h2: {
-        marginTop: '3rem',
-      },
-      h3: {
-        marginTop: '3rem',
-      },
-      h4: {
-        marginTop: '3rem',
-      }
-    },
     theme: {
       extend: {},
     },
   },
-  plugins: [
-    require('daisyui'),
-    tailwindMdBase(),
-  ],
-  daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
-  },
+  plugins: [],
 }
